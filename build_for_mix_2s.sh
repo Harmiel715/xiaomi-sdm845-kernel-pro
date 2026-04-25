de8020376e813458a5cdead1e94111a911e77c0e
huangdihd <hd20100104@163.com>
2026-04-25 13:56:50 +0800

    Integrate susfs

---
+++ build_for_mix_2s.sh
@@ -0,0 +1,10 @@
+export ARCH=arm64
+export SUBARCH=arm64
+export PATH="$HOME/zyc-clang19/bin:$PATH"
+
+make O=out ARCH=arm64 clean
+make O=out ARCH=arm64 mrproper
+make O=out ARCH=arm64 vendor/xiaomi/mi845_defconfig
+scripts/kconfig/merge_config.sh -O out/ out/.config arch/arm64/configs/vendor/xiaomi/polaris.config
+make -j$(nproc) O=out ARCH=arm64 CC="clang -fuse-ld=lld" LD=ld.lld CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- CROSS_COMPILE_COMPAT=arm-linux-gnueabi- LLVM_IAS=1 KCFLAGS="-Wno-error"
+
